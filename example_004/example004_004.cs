3f39a247efb164139ffb802fa073c0163716ca1c
Anna <an8448@mail.ru>
2022-09-21 21:12:48 +0500

    example_004

---
+++ example_004/example004_004.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+int A=6;
+int B=3;
+int C=-2;
+
+if(A > B)
+{
+    if(A > C)
+    {
+        Console.Write("Наибольшее число ");
+        Console.Write(A);
+    }
+    else
+        Console.Write("Наибольшее число ");
+        Console.Write(C);
+}
+else
+    if(B > C)
+    {
+        Console.Write("Наибольшее число ");
+        Console.Write(B);
+    }
+    else
+        Console.Write("Наибольшее число ");
+        Console.Write(C);
+
