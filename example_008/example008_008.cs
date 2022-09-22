06a28a3e2e708575f612086a0f19df3a5eb01aa2
Anna <an8448@mail.ru>
2022-09-22 10:21:56 +0500

    example008

---
+++ example_008/example008_008.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Введите число");
+int finish = Convert.ToInt32(Console.Read());
+    for (int i = 1; i < finish; i++)
+    {
+        if ((i % 2 == 0) && !(i==1))
+        {
+            Console.WriteLine(" " + Convert.ToInt32(i));
+        }
+    }
