9e10289b080d1d9a19dbf499385115d672f3d93c
Anna <an8448@mail.ru>
2022-10-21 21:16:50 +0500

    example 064

--- example_064/Program.cs
+++ example_064/Program.cs
@@ -1,33 +1,40 @@
+/*
+ Задача 64: Задайте значения N и M. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+M = 1; N = 5 -> 2, 4
+M = 4; N = 8 -> 4, 6, 8
+ */
+
 Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число N: ");
+Console.Write("Введите число N, больше М: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-NaturalToLow(m, n);
+NaturalToLow(n, m);
 
-void numbers (int m, int n)
+void numbers (int n, int m)
 {
-     if (n >m)
+     if (m > n)
     {
         return;
     }
     else
     {
-        if (n % 2 == 0)
-        Console.Write(n + " ");
+        if (m % 2 == 0)
+        Console.Write(m + " ");
     }
 }
 
-void NaturalToLow(int m, int n)
+void NaturalToLow(int n, int m)
 {
-    if (n > m)
+    if (m > n)
     {
         return;
     }
     else
     {
-        NaturalToLow(m, n + 1);
-        numbers(m, n);
+        NaturalToLow(n, m + 1);
+        numbers(n, m);
     }
 }
